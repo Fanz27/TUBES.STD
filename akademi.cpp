3b1ffefa4d57645a2ddb0138f325e9dfc3b1398f
Fanz27 <fatihnovendi86@gmail.com>
2024-12-03 14:15:21 +0700

    first commit

---
+++ akademi.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "akademi.h"
+
+using namespace std;
+
+void createList(List_MLL &L){
+    first(L) = NULL;
+    last(L) = NULL;
+}
+
+void createListDivisi(List_MLL &L){
+    first(L) = NULL;
+}
+
+adr_Kategori createNewKategori(infoK data){
+    adr_Kategori P = new elemenKategori;
+    info(P) = data;
+    next(P) = NULL;
+    last(P) = NULL;
+
+    return P;
+}
+
+void insertKategoriLast(List_MLL &L, adr_Kategori P){
+    createList(L);
+    if (first(L) == NULL) {
+        first(L) = P;
+    } else {
+        adr_Kategori temp = first(L);
+        while (next(temp) != NULL) {
+            temp = next(temp);
+        }
+        next(temp) = P;
+    }
+
+}
+void deleteKategoriLast(List_MLL &L, adr_Kategori &P, adr_Kategori prec){
+    insertKategoriLast(L, P);
+    if (insertKategoriLast(L, P) == NULL) {
+        cout << "Data kosong tidak bisa dihapus" << endl;
+    } else {
+        prec = infoKategori;
+        P = next(prec);
+        next(prec) = next(P);
+        next(P) = NULL;
+    }
+}
+void printKategori(List_MLL &L){
+
+}
+
+adr_Divisi createNewDivisi(infoD data){
+
+}
+void insertDivisiLast(listDivisi &L, adr_Divisi P){
+
+}
+void deleteDivisiFirst(listDivisi &L, adr_Divisi &P){
+
+}
+void deleteDivisiLast(listDivisi &L, adr_Divisi &P){
+
+}
+void printDivisi(listDivisi &L){
+
+}
+
+void addDivisiToKategori(adr_Kategori &K, adr_Divisi P){
+
+}
+void removeDivisiFromKategori(adr_Kategori &K, adr_Divisi &P){
+
+}
---
+++ akademi.h
@@ -0,0 +1,82 @@
+#ifndef AKADEMI_H_INCLUDED
+#define AKADEMI_H_INCLUDED
+#include <iostream>
+
+#define infoKategori(P) (P)->infoKategori
+#define nextKategori(P) (P)->nextKategori
+#define prevKategori(P) (P)->prevKategori
+#define nextDiv(P) (P)->nextDiv
+#define next(temp) (temp)->next
+#define infoDivisi(P) (P)->infoDivisi
+#define first(L) (L).first
+#define last(L) (L).last
+
+using namespace std;
+
+//Kategori
+struct kategori {
+    string namaK;
+    string desK;
+    bool jenis;
+};
+
+typedef struct elemenKategori *adr_Kategori;
+typedef kategori infoK;
+
+struct elemenKategori{
+    infoK infoKategori;
+    adr_Kategori nextKategori;
+    adr_Kategori prevKategori;
+    adr_Divisi first;
+};
+
+
+//Divisi
+struct divisi{
+    string namaD;
+    string durasi;
+    string harga;
+    string instruktur;
+};
+typedef struct elemenDivisi *adr_Divisi;
+typedef divisi infoD;
+
+struct elemenDivisi{
+    infoD infoDivisi;
+    adr_Divisi nextDiv;
+};
+
+
+
+struct List_MLL{
+    adr_Kategori first;
+    adr_Kategori last;
+};
+
+
+void createList(List_MLL &L);
+
+// untuk elemen kategori
+adr_Kategori createNewKategori(infoK data);
+void insertKategoriLast(List_MLL &L, adr_Kategori P);
+void deleteKategoriLast(List_MLL &L, adr_Kategori &P);
+adr_Kategori searchingKategori(List_MLL &L, string jenis);
+void printKategori(List_MLL &L);
+
+// Divisi Management
+adr_Divisi createNewDivisi(infoD data);
+void insertDivisiLast(List_MLL &L, adr_Divisi P);
+void deleteDivisiFirst(List_MLL &L, adr_Divisi &P);
+void deleteDivisiLast(List_MLL &L, adr_Divisi &P);
+void printDivisi(List_MLL &L);
+
+// Add and Remove Divisi to/from Kategori
+void addDivisiToKategori(adr_Kategori &K, adr_Divisi P);
+void removeDivisiFromKategori(adr_Kategori &K, adr_Divisi &P);
+
+//menampilkan seluruh data kategori bersama divisinya
+void showAll(List_MLL &L);
+
+void listMenu();
+
+#endif // AKADEMI_H_INCLUDED
