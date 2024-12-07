a15e72b67ac0d0f869102b98416bb44c7da63dc4
Fanz27 <fatihnovendi86@gmail.com>
2024-12-07 12:23:19 +0700

    terbaru

--- akademi.cpp
+++ akademi.cpp
@@ -1,155 +1,155 @@
-#include <iostream>
-#include "akademi.h"
+    #include <iostream>
+    #include "akademi.h"
 
-using namespace std;
+    using namespace std;
 
-void createList(List_MLL &L){
-    first(L) = NULL;
-    last(L) = NULL;
-}
-
-void createListDivisi(List_MLL &L){
-    first(L) = NULL;
-}
-
-adr_Kategori createNewKategori(infoK data){
-    adr_Kategori P = new elemenKategori;
-    infoKategori(P) = data;
-    nextKategori(P) = NULL;
-    last(P) = NULL;
-
-    return P;
-}
-
-void insertKategoriLast(List_MLL &L, adr_Kategori P){
-    if (first(L) == NULL) {
-        first(L) = P;
-        last(L) = P;
-        nextKategori(P) = NULL;
-        prevKategori(P) = NULL;
-    } else {
-        adr_Kategori temp = last(L);
-        nextKategori(temp) = P;
-        prevKategori(P) = temp;
-        nextKategori(P) = NULL;
-        last(L) = P;
-    }
-
-}
-void deleteKategoriLast(List_MLL &L, adr_Kategori &P, adr_Kategori prec){
-    if (first(L) == NULL) {
-        cout << "Data kosong tidak bisa dihapus" << endl;
-        P = NULL;
-    } else if (first(L) == last(L)) {
-        P = first(L);
+    void createList(List_MLL &L){
         first(L) = NULL;
         last(L) = NULL;
-    } else {
-        adr_Kategori temp = first(L);
-        adr_Kategori prec = NULL;
-        while (nextKategori(temp)!= NULL) {
-            prec = temp;
-            temp = nextKategori(temp);
-        }
-        P = temp;
-        nextKategori(prec) = NULL;
-    }
-}
-void printKategori(List_MLL &L){
-    adr_Kategori P = first(L);
-    while (P != NULL) {
-        cout << infoKategori(P).namaK << " ";
-        cout << infoKategori(P).desK << " ";
-        cout << infoKategori(P).jenis << " ";
-        P = nextKategori(P);
-    }
-    cout << endl;
-}
-
-adr_Divisi createNewDivisi(infoD data){
-    adr_Divisi Q = new elemenDivisi;
-    infoDivisi(Q) = data;
-    nextDiv(Q) = NULL;
-
-    return Q;
-}
-void insertDivisiLast(List_MLL &L, adr_Divisi P){
-    adr_Kategori K = last(L);
-    if (nextDiv(K) == NULL){
-        nextDiv(K) = P;
-    } else {
-        adr_Divisi Q = nextDiv(K);
-        while (nextDiv(Q) != NULL) {
-            Q = nextDiv(Q);
-        }
-        nextDiv(Q) = P;
     }
 
-}
-void deleteDivisiFirst(listDivisi &L, adr_Divisi P) {
-    P = first(L);
-    first(L) = nextDiv(P);
-    nextDiv(P) = NULL;
-
-    return P;
-}
-void deleteDivisiLast(List_MLL &L, adr_Divisi &P) {
-    if (first(L) == NULL) {
-        cout << "Data kosong tidak bisa dihapus" << endl;
-        P = NULL;
-    } else if (first(L) == last(L)) {
-        P = first(L);
+    void createListDivisi(List_MLL &L){
         first(L) = NULL;
-        last(L) = NULL;
-    } else {
-        adr_Divisi Q = first(L);
-        adr_Divisi prec = NULL;
-        while (nextDiv(Q)!= NULL) {
-            prec = temp;
-            Q = nextDiv(Q);
-        }
-        P = Q;
-        nextDiv(prec) = NULL;
-        last(L) = NULL;
     }
-}
-void printDivisi(List_MLL &L){
-    adr_Kategori P = first(L);
-    while (P != NULL) {
-        adr_Divisi Q = nextDiv(P);
-        cout << "  Divisi for Kategori: " << infoKategori(P).namaK << endl;
-        while (Q != nullptr) {
-            cout << "    Divisi: " << infoDivisi(Q).namaD << endl;
-            cout << "    Durasi: " << infoDivisi(Q).durasi << endl;
-            cout << "    Harga: " << infoDivisi(Q).harga << endl;
-            cout << "    Instruktur: " << infoDivisi(Q).instruktur << endl;
-            Q = nextDiv(Q);
-        }
-        P = nextKategori(P);
+
+    adr_Kategori createNewKategori(infoK data){
+        adr_Kategori P = new elemenKategori;
+        infoKategori(P) = data;
+        nextKategori(P) = NULL;
+        last(P) = NULL;
+
+        return P;
     }
-}
 
-void addDivisiToKategori(adr_Kategori &K, adr_Divisi P){
+    void insertKategoriLast(List_MLL &L, adr_Kategori P){
+        if (first(L) == NULL) {
+            first(L) = P;
+            last(L) = P;
+            nextKategori(P) = NULL;
+            prevKategori(P) = NULL;
+        } else {
+            adr_Kategori temp = last(L);
+            nextKategori(temp) = P;
+            prevKategori(P) = temp;
+            nextKategori(P) = NULL;
+            last(L) = P;
+        }
 
-}
-void removeDivisiFromKategori(adr_Kategori &K, adr_Divisi &P){
-    
-}
+    }
+    void deleteKategoriLast(List_MLL &L, adr_Kategori &P, adr_Kategori prec){
+        if (first(L) == NULL) {
+            cout << "Data kosong tidak bisa dihapus" << endl;
+            P = NULL;
+        } else if (first(L) == last(L)) {
+            P = first(L);
+            first(L) = NULL;
+            last(L) = NULL;
+        } else {
+            adr_Kategori temp = first(L);
+            adr_Kategori prec = NULL;
+            while (nextKategori(temp)!= NULL) {
+                prec = temp;
+                temp = nextKategori(temp);
+            }
+            P = temp;
+            nextKategori(prec) = NULL;
+        }
+    }
+    void printKategori(List_MLL &L){
+        adr_Kategori P = first(L);
+        while (P != NULL) {
+            cout << infoKategori(P).namaK << " ";
+            cout << infoKategori(P).desK << " ";
+            cout << infoKategori(P).jenis << " ";
+            P = nextKategori(P);
+        }
+        cout << endl;
+    }
 
-void showAll(List_MLL &L){
-    adr_Kategori P = first(L);
-    while (P != NULL){
-        cout << "kategori  : " << infoKategori(P).namaK << endl;
-        cout << "Deskripsi : " << infoKategori(P).desK << endl;
-        cout << "kategori  : " << infoKategori(P).jenis << endl;
-        adr_Divisi = nextDiv(P);
-        while (Q != NULL){
-                cout << "Divisi     : " << infoDivisi(P).namaD << endl;
-                cout << "Durasi     : " << infoDivisi(P).durasi << endl;
-                cout << "Harga      : " << infoDivisi(P).harga << endl;
-                cout << "Instruktur : " << infoDivisi(P).instruktur << endl;
+    adr_Divisi createNewDivisi(infoD data){
+        adr_Divisi Q = new elemenDivisi;
+        infoDivisi(Q) = data;
+        nextDiv(Q) = NULL;
+
+        return Q;
+    }
+    void insertDivisiLast(List_MLL &L, adr_Divisi P){
+        adr_Kategori K = last(L);
+        if (nextDiv(K) == NULL){
+            nextDiv(K) = P;
+        } else {
+            adr_Divisi Q = nextDiv(K);
+            while (nextDiv(Q) != NULL) {
                 Q = nextDiv(Q);
+            }
+            nextDiv(Q) = P;
         }
-        P = nextKategori(P);
+
     }
-}
+    void deleteDivisiFirst(listDivisi &L, adr_Divisi P) {
+        P = first(L);
+        first(L) = nextDiv(P);
+        nextDiv(P) = NULL;
+
+        return P;
+    }
+    void deleteDivisiLast(List_MLL &L, adr_Divisi &P) {
+        if (first(L) == NULL) {
+            cout << "Data kosong tidak bisa dihapus" << endl;
+            P = NULL;
+        } else if (first(L) == last(L)) {
+            P = first(L);
+            first(L) = NULL;
+            last(L) = NULL;
+        } else {
+            adr_Divisi Q = first(L);
+            adr_Divisi prec = NULL;
+            while (nextDiv(Q)!= NULL) {
+                prec = temp;
+                Q = nextDiv(Q);
+            }
+            P = Q;
+            nextDiv(prec) = NULL;
+            last(L) = NULL;
+        }
+    }
+    void printDivisi(List_MLL &L){
+        adr_Kategori P = first(L);
+        while (P != NULL) {
+            adr_Divisi Q = nextDiv(P);
+            cout << "  Divisi for Kategori: " << infoKategori(P).namaK << endl;
+            while (Q != nullptr) {
+                cout << "    Divisi: " << infoDivisi(Q).namaD << endl;
+                cout << "    Durasi: " << infoDivisi(Q).durasi << endl;
+                cout << "    Harga: " << infoDivisi(Q).harga << endl;
+                cout << "    Instruktur: " << infoDivisi(Q).instruktur << endl;
+                Q = nextDiv(Q);
+            }
+            P = nextKategori(P);
+        }
+    }
+
+    void addDivisiToKategori(adr_Kategori &K, adr_Divisi P){
+
+    }
+    void removeDivisiFromKategori(adr_Kategori &K, adr_Divisi &P){
+        
+    }
+
+    void showAll(List_MLL &L){
+        adr_Kategori P = first(L);
+        while (P != NULL){
+            cout << "kategori  : " << infoKategori(P).namaK << endl;
+            cout << "Deskripsi : " << infoKategori(P).desK << endl;
+            cout << "kategori  : " << infoKategori(P).jenis << endl;
+            adr_Divisi = nextDiv(P);
+            while (Q != NULL){
+                    cout << "Divisi     : " << infoDivisi(P).namaD << endl;
+                    cout << "Durasi     : " << infoDivisi(P).durasi << endl;
+                    cout << "Harga      : " << infoDivisi(P).harga << endl;
+                    cout << "Instruktur : " << infoDivisi(P).instruktur << endl;
+                    Q = nextDiv(Q);
+            }
+            P = nextKategori(P);
+        }
+    }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,25 @@
 
 using namespace std;
 
-int main(){
-    cout << "TEST" << endl;
-    return 0;
+#include <iostream>
+#include "akademi.h"
+
+using namespace std;
+
+void showMainMenu() {
+    cout << "==== Main Menu ====" << endl;
+    cout << "1. Create new Kategori" << endl;
+    cout << "2. Create new Divisi" << endl;
+    cout << "3. Add Divisi to Kategori" << endl;
+    cout << "4. Delete Divisi from Kategori" << endl;
+    cout << "5. Print all Categories and Divisis" << endl;
+    cout << "6. Delete last Divisi" << endl;
+    cout << "7. Show All Data" << endl;
+    cout << "8. Exit" << endl;
+    cout << "====================" << endl;
+    cout << "Choose an option: ";
+}
+
+int main() {
+    cout << "Nama Kamu: " << endl;
 }
